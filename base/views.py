6995e5391e7272874e3a1bf7203ae0ec10384763
Koboju Kibeyefa <kobojuezekiel@gmail.com>
2022-10-27 10:01:08 +0100

    Second commit

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+
+
+class SlackDetailsView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'slackUsername': 'kibeyefa',
+            'backend': True,
+            'age': 21,
+            'bio': "",
+        }
+
+        return Response(data=data)
