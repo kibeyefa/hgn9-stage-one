1e627fb26dcc56d1326ab24305b288d180fb575b
kibeyefa <61443573+kibeyefa@users.noreply.github.com>
2022-10-28 15:08:02 +0100

    added whitenoise settings

--- stage_one/settings.py
+++ stage_one/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-
+	"whitenoise.middleware.WhiteNoiseMiddleware", 
     "corsheaders.middleware.CorsMiddleware",
 
     'django.middleware.common.CommonMiddleware',
@@ -128,6 +128,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIR = [
 	'static', 
 ] 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
