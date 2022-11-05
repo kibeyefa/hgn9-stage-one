c08f7d7588d1cc75dbc5e31f148e5a752f2a9914
Koboju Kibeyefa <kobojuezekiel@gmail.com>
2022-11-05 09:43:00 +0100

    ..

--- base/views.py
+++ base/views.py
@@ -6,15 +6,30 @@ from rest_framework.response import Response
 
 
 class SlackDetailsView(APIView):
-    def get(self, request, *args, **kwargs):
-        data = {
-            'slackUsername': 'kibeyefa',
-            'backend': True,
-            'age': 21,
-            'bio': "I am Koboju Kibeyefa, a backend developer and student of The Federal University of Technology Akure.",
-        }
+    data = {
+        "slackUsername": "kibeyefa",
+        "backend": True,
+        "age": 21,
+        "bio": "I am Koboju Kibeyefa, a backend developer and student of The Federal University of Technology Akure.",
+    }
 
-        return Response(data=data)
+    def get(self, request, *args, **kwargs):
+        return Response(data=self.data)
 
     def post(self, *args, **wargs):
-        pass
+        post_data = self.request.data
+        operation = post_data.get('operatation_type')
+        x: int = post_data.get('x')
+        y: int = post_data.get('y')
+
+        result: int = 0
+
+        if operation == 'addition':
+            result = x + y
+        elif operation == 'subtraction':
+            result = x - y
+        elif operation == 'multiplication':
+            result = int(x * y)
+
+        self.data['result'] = result
+        return Response(self.data)
